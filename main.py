d3909dde0622b26ce6e1b9e1299571513f60352a
Mahedul-Karim <karimrupo21@gmail.com>
2026-04-16 17:04:37 +0600

    label and button added

--- main.py
+++ main.py
@@ -21,10 +21,19 @@ window = Tk()
 window.title("pomodoro")
 window.config(padx=100,pady=50,bg=YELLOW)
 
+label = Label(text="Timer",fg=GREEN, bg=YELLOW,font=(FONT_NAME,50))
+label.grid(column=1,row=0)
+
 canvas = Canvas(width=200,height=224,bg=YELLOW,highlightthickness=0)
 img = PhotoImage(file="tomato.png")
 canvas.create_image(100,112,image=img)
 canvas.create_text(100,130,text="00:00",fill="white",font=(FONT_NAME,35,"bold"))
-canvas.pack()
+canvas.grid(column=1,row=1)
+
+start_button = Button(text="Start")
+start_button.grid(column=0,row=2)
+
+reset_button = Button(text="Reset")
+reset_button.grid(column=2,row=2)
 
 window.mainloop()
